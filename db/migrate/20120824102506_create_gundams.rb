7b2e7bc25b79bfc239eac0b735cf2448e95e956c
Joey wang <wxluckly@gmail.com>
2013-01-17 23:35:41 +0800

    add code

--- db/migrate/20120824102506_create_gundams.rb
+++ db/migrate/20120824102506_create_gundams.rb
@@ -3,7 +3,7 @@ class CreateGundams < ActiveRecord::Migration
 		create_table :gundams do |t|
       t.string :series          # 年代
       t.string :series_name     # 年代名称
-			t.string :name
+			t.string :name    
       t.string :name_chs
       t.string :model
       t.string :type_id         # 类型

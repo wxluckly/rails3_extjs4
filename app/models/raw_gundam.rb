01418a8e43ac0d314fd73cacdb22fefa27c8fb54
Joey wang <wxluckly@gmail.com>
2013-01-18 14:56:31 +0800

    coding

--- app/models/raw_gundam.rb
+++ app/models/raw_gundam.rb
@@ -1,3 +1,3 @@
 class RawGundam < ActiveRecord::Base
-  attr_accessible :name, :name_chs, :model, :period, :name_jp, :story, :usage, :manufactory, :force, :period, :specifications, :internal_environment, :measurement, :weight, :armor, :output, :propulsion, :acceleration, :special_equipped, :default_weapon, :selected_weapon, :hand_weapon, :ranged_weapon
+  attr_accessible :id, :period, :name, :name_chs, :name_jp, :model, :usage, :force, :story, :manufactory, :specifications, :internal_environment, :measurement, :weight, :armor, :output, :propulsion, :acceleration, :special_equipped, :default_weapon, :selected_weapon, :hand_weapon, :ranged_weapon, :summary
 end

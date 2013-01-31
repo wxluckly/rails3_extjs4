89636f92f34f5be1581909da2707b4c4dc1bc58e
freebird0221 <freebird0221@gmail.com>
2013-01-31 09:58:04 +0800

    version coding

---
+++ app/models/gundam_version.rb
@@ -0,0 +1,39 @@
+class GundamVersion < ActiveRecord::Base
+  attr_accessible :gundam_id, :user_id, :model, :name, :name_chs, :name_jp, :usage_id, :period_id, :story_id, :manufactory_id,
+    :specifications, :internal_environment, :measurement, :weight, :armor, :output, :propulsion, :acceleration, :special_equipped,
+    :default_weapon, :selected_weapon, :hand_weapon, :ranged_weapon, :summary
+
+  belongs_to  :gundam
+  belongs_to  :user
+  belongs_to  :usage
+  belongs_to  :period
+  belongs_to  :story
+  belongs_to  :manufactory
+
+  validates :gunadm_id, :presence
+  validate :not_all_nil
+
+  before_save :clear_unchanged_column
+
+  def not_all_nil
+    (attribute_names - ["id", "gundam_id", "user_id", "created_at", "updated_at"]).each do |attr_name|
+      return if gundam.send(attr_name) != send(attr_name)
+    end
+    (attribute_names - ["id", "gundam_id", "user_id", "created_at", "updated_at"]).each do |attr_name|
+      errors.add(:attr_name, '不能所有属性都没有修改')
+    end
+  end
+
+  def clear_unchanged_column
+    (attribute_names - ["id", "gundam_id", "user_id", "created_at", "updated_at"]).each do |attr_name|
+      send("#{attr_name}=",nil) if gundam.send(attr_name) == send(attr_name)
+    end
+  end
+
+  def merge
+    (attribute_names - ["id", "gundam_id", "user_id", "created_at", "updated_at"]).each do |attr_name|
+      gundam.send("#{attr_name}=",send(attr_name)) unless send(attr_name).nil?
+    end
+    gundam.save
+  end
+end

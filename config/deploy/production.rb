ce381aadc4cd7750ac871554e60138aded31a9bb
Joey wang <wxluckly@gmail.com>
2013-07-07 18:21:38 +0800

    try to deploy

---
+++ config/deploy/production.rb
@@ -0,0 +1,15 @@
+set :rails_env, 'production'
+
+set :branch, 'master'
+
+# 代码发布服务器
+role :app, '115.28.37.253'
+
+# 网站服务器
+role :web, '115.28.37.253'
+
+# 后台任务服务器
+role :bg, '115.28.37.253'
+
+# 使用数据库服务器运行迁移，当前所用地址为HA的虚拟IP
+role :db,  '115.28.37.253', primary: true, no_release: true

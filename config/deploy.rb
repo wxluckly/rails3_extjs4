0717eba1294f7f73b23d05266e88c1e2aa28f4ae
Joey wang <wxluckly@gmail.com>
2013-07-09 10:58:41 +0800

    add precompile

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,8 +33,44 @@ before('deploy:finalize_update', 'deploy:links')
 #   end
 # end
 
-namespace :deploy do
+namespace :bluepill do
+  #确认问题：
+  #  创建 /var/run/bluepill被创建并属于www用户？
+  task :prepare, :role=>:web do
+    set :user, 'railsu'
+    default_run_options[:pty] = true #for remote interaction
+    sudo "mkdir -p /var/run/bluepill"
+    sudo "chown railsu:railsu /var/run/bluepill"
+    puts "Prepare bluepill to monitor unicorn... "
+  end
+
+  task :start, :roles => :web do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec bluepill --no-privileged load #{current_path}/config/bluepill.rb"
+  end
   
+  task :stop do
+    run "cd #{current_path} && bundle exec bluepill --no-privileged quit"
+  end
+end
+
+namespace :deploy do
+
+  # 重启服务
+  task :start do
+    run "cd #{current_path} && bundle exec bluepill --no-privileged start unicorn RAILS_ENV=#{rails_env}"
+  end
+
+  task :stop do
+    run "cd #{current_path} && bundle exec bluepill --no-privileged stop unicorn RAILS_ENV=#{rails_env}"
+  end
+  
+  task :restart, :roles => :web do
+    if rails_env == "production"
+      run "cd #{current_path} && bundle exec bluepill --no-privileged restart unicorn RAILS_ENV=#{rails_env}"
+    else
+      deploy.restart_unicorn
+    end
+  end
 
   task :links, :roles => :app do
     # 建立配置文件链接
